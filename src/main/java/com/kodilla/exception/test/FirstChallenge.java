317096642129d8bb22f128070fe7dd2069177364
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-04-14 13:47:15 +0100

    try/catch/finally

--- src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,5 +1,10 @@
 package com.kodilla.exception.test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
@@ -9,15 +14,15 @@ public class FirstChallenge {
         return a / b;
     }
 
-    /**
-     * This main can throw an ArithmeticException!!!
-     * @param args
-     */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
 
-
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Oh no! 0??");
+        } finally {
+            System.out.println("done");
+        }
     }
 }
